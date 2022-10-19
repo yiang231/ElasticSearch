fd62eb15dd91af6616296df2124e4bbb24b8a9a4
Yiang <1712064922@qq.com>
2022-10-19 15:23:27 +0800

    ElasticSearch:范围查询rangeQuery

--- src/test/java/com/atguigu/TestQuery.java
+++ src/test/java/com/atguigu/TestQuery.java
@@ -10,11 +10,13 @@ import org.elasticsearch.index.query.MatchAllQueryBuilder;
 import org.elasticsearch.index.query.MatchQueryBuilder;
 import org.elasticsearch.index.query.Operator;
 import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.query.RangeQueryBuilder;
 import org.elasticsearch.index.query.TermQueryBuilder;
 import org.elasticsearch.index.query.WildcardQueryBuilder;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHits;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.search.sort.SortOrder;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -157,6 +159,41 @@ public class TestQuery {
 		}
 		//遍历List<Goods>输出
 		goodsArrayList.forEach(System.out::println);//57
+		System.out.println(goodsArrayList.size());//Java中goodList集合
+	}
+
+	@Test
+	public void testRange() throws IOException {
+		//创建查询的请求
+		SearchRequest request = new SearchRequest("goods");
+		//分页
+		SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+		//条件
+		RangeQueryBuilder queryBuilder = QueryBuilders.rangeQuery("price").gte(2000).lte(3000);
+		searchSourceBuilder.query(queryBuilder);
+		request.source(searchSourceBuilder);
+
+		searchSourceBuilder.from(0);
+		searchSourceBuilder.size(20);//默认值10
+		searchSourceBuilder.sort("price", SortOrder.DESC);//排序
+		//执行查询请求并且得到响应
+		SearchResponse response = client.search(request, RequestOptions.DEFAULT);
+		//将结果封装到List<Goods>中
+		List<Goods> goodsArrayList = new ArrayList<>();
+		SearchHits hits = response.getHits();
+		long totalHits = hits.getTotalHits().value;
+		System.out.println("totalHits = " + totalHits);//ES中文档数量，goods索引的总数量
+
+		SearchHit[] hitsArr = hits.getHits();
+		for (SearchHit hit : hitsArr) {
+			//获取文档字符串
+			String hitSourceAsString = hit.getSourceAsString();
+			//将文档字符串转换为goods对象
+			Goods goods = JSON.parseObject(hitSourceAsString, Goods.class);
+			goodsArrayList.add(goods);
+		}
+		//遍历List<Goods>输出
+		goodsArrayList.forEach(System.out::println);//172
 		System.out.println(goodsArrayList.size());//Java中goodList集合
 	}
 }
