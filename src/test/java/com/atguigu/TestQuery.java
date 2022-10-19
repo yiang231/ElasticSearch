8a91f7e3c168098c6dbbf9aa06acd70d6b9fdec5
Yiang <1712064922@qq.com>
2022-10-19 15:03:14 +0800

    ElasticSearch:模糊查询wildcardQuery

--- src/test/java/com/atguigu/TestQuery.java
+++ src/test/java/com/atguigu/TestQuery.java
@@ -11,6 +11,7 @@ import org.elasticsearch.index.query.MatchQueryBuilder;
 import org.elasticsearch.index.query.Operator;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.index.query.TermQueryBuilder;
+import org.elasticsearch.index.query.WildcardQueryBuilder;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHits;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
@@ -38,7 +39,7 @@ public class TestQuery {
 		request.source(searchSourceBuilder);
 
 		searchSourceBuilder.from(0);
-		searchSourceBuilder.size(10);//默认值
+		searchSourceBuilder.size(10);//默认值10
 		//执行查询请求并且得到响应
 		SearchResponse response = client.search(request, RequestOptions.DEFAULT);
 		//将结果封装到List<Goods>中
@@ -56,7 +57,7 @@ public class TestQuery {
 			goodsArrayList.add(goods);
 		}
 		//遍历List<Goods>输出
-		goodsArrayList.forEach(System.out::println);
+		goodsArrayList.forEach(System.out::println);//940
 		System.out.println(goodsArrayList.size());//Java中goodList集合
 	}
 
@@ -71,7 +72,7 @@ public class TestQuery {
 		request.source(searchSourceBuilder);
 
 		searchSourceBuilder.from(0);
-		searchSourceBuilder.size(20);//默认值
+		searchSourceBuilder.size(20);//默认值10
 		//执行查询请求并且得到响应
 		SearchResponse response = client.search(request, RequestOptions.DEFAULT);
 		//将结果封装到List<Goods>中
@@ -89,7 +90,7 @@ public class TestQuery {
 			goodsArrayList.add(goods);
 		}
 		//遍历List<Goods>输出
-		goodsArrayList.forEach(System.out::println);
+		goodsArrayList.forEach(System.out::println);//51
 		System.out.println(goodsArrayList.size());//Java中goodList集合
 	}
 
@@ -104,7 +105,7 @@ public class TestQuery {
 		request.source(searchSourceBuilder);
 
 		searchSourceBuilder.from(0);
-		searchSourceBuilder.size(20);//默认值
+		searchSourceBuilder.size(20);//默认值10
 		//执行查询请求并且得到响应
 		SearchResponse response = client.search(request, RequestOptions.DEFAULT);
 		//将结果封装到List<Goods>中
@@ -122,7 +123,40 @@ public class TestQuery {
 			goodsArrayList.add(goods);
 		}
 		//遍历List<Goods>输出
-		goodsArrayList.forEach(System.out::println);
+		goodsArrayList.forEach(System.out::println);//728
+		System.out.println(goodsArrayList.size());//Java中goodList集合
+	}
+
+	@Test
+	public void testWildcard() throws IOException {
+		//创建查询的请求
+		SearchRequest request = new SearchRequest("goods");
+		//分页
+		SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+		WildcardQueryBuilder queryBuilder = QueryBuilders.wildcardQuery("title", "华*");
+		searchSourceBuilder.query(queryBuilder);
+		request.source(searchSourceBuilder);
+
+		searchSourceBuilder.from(0);
+		searchSourceBuilder.size(20);//默认值10
+		//执行查询请求并且得到响应
+		SearchResponse response = client.search(request, RequestOptions.DEFAULT);
+		//将结果封装到List<Goods>中
+		List<Goods> goodsArrayList = new ArrayList<>();
+		SearchHits hits = response.getHits();
+		long totalHits = hits.getTotalHits().value;
+		System.out.println("totalHits = " + totalHits);//ES中文档数量，goods索引的总数量
+
+		SearchHit[] hitsArr = hits.getHits();
+		for (SearchHit hit : hitsArr) {
+			//获取文档字符串
+			String hitSourceAsString = hit.getSourceAsString();
+			//将文档字符串转换为goods对象
+			Goods goods = JSON.parseObject(hitSourceAsString, Goods.class);
+			goodsArrayList.add(goods);
+		}
+		//遍历List<Goods>输出
+		goodsArrayList.forEach(System.out::println);//57
 		System.out.println(goodsArrayList.size());//Java中goodList集合
 	}
 }
